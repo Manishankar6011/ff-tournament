d589c7d3122fe859a13518b365e900a957bab168
Manishankar6011 <manishankar6011@gmail.com>
2026-08-08 18:37:30 +0530

    fix: remove next-pwa and make supabase client build-safe

--- next.config.ts
+++ next.config.ts
@@ -54,11 +54,4 @@ const nextConfig = {
   },
 }
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-})
-
-module.exports = withPWA(nextConfig)
+export default nextConfig
